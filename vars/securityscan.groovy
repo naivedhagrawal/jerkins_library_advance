b21b6b7014983fdb059fa55b2f0fea16fd28d7fb
naivedh <naivedh@gmail.com>
2025-03-29 18:01:13 +0530

    1

--- vars/securityscan.groovy
+++ vars/securityscan.groovy
@@ -112,10 +112,10 @@ def call(Map params = [:]) {
                 recordIssues(
                     enabledForFailure: true,
                     tools: [
-                        sarif(pattern: "gitleaks-report.sarif", id: "Secrets", name: "Secret Scanning Report", icon: "lock"),
-                        sarif(pattern: "semgrep-report.sarif", id: "StaticAnalysis", name: "Static Analysis Report", icon: "code"),
-                        sarif(pattern: "results.sarif", id: "IaC", name: "IaC Vulnerability Report", icon: "cloud"),
-                        owaspDependencyCheck(pattern: "owasp-report.json", id: "Vulnerable Dependencies", name: "Dependency Check Report", icon: "shield")
+                        sarif(pattern: "gitleaks-report.sarif", id: "Gitleaks", name: "Secret Scanning Report"),
+                        sarif(pattern: "semgrep-report.sarif", id: "Semgrep", name: "Static Analysis Report"),
+                        sarif(pattern: "results.sarif", id: "Checkov", name: "IaC Vulnerability Report"),
+                        owaspDependencyCheck(pattern: "owasp-report.json", id: "OWASP", name: "Dependency Check Report")
                     ]
                 )
             }
